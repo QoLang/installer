86573ab9608ce934babdc3fa1bef37240363354e
Akif9748 <akif9748@gmail.com>
2022-10-30 20:46:34 +0300

    Added nt, and not supported message is updated.

--- qolang-installer.py
+++ qolang-installer.py
@@ -69,8 +69,68 @@ if os.name == "posix":
         print("---> Done!")
 
 elif os.name == "nt":
-    print("Windows is not supported yet. Please follow the instructions on the website.")
-    exit(1)
+    WIN_QO = "C:\\qolang"
+
+    if not os.access("C:\\", os.W_OK):
+        print("--!> C:\\ is not writeable, run as root")
+        exit(1)
+
+    instype = input("--?> Install from git (requires git)? [Y/n] ").lower() != "n"
+
+    if instype:
+        if os.system("git --version") != 0:
+            print("--!> Installing with git requires git, git is not installed")
+            exit(1)
+
+        if os.path.isdir(WIN_QO):
+            print("---> Removing old qolang")
+            try:
+                shutil.rmtree(WIN_QO)
+            except:
+                print("--!> Failed to remove old qolang, delete it manually from", WIN_QO)
+                exit(1)
+                       
+        print("---> Creating", WIN_QO)
+        os.mkdir(WIN_QO)
+        print("---> Cloning the git repository to", WIN_QO)
+        os.system("git clone https://github.com/QoLang/QoLang "+WIN_QO)
+        print("---> Downloading QoLang binary")
+        urllib.request.urlretrieve(
+            "https://github.com/QoLang/QoLang/releases/latest/download/qo.exe", WIN_QO+"\\qo.exe")
+
+        print("---> Adding qolang to PATH")
+        os.system("setx PATH %PATH%;"+WIN_QO)    
+        print("---> Done!")
+
+    else:
+        if os.path.isdir(WIN_QO):
+            print("---> Removing old qolang")
+            shutil.rmtree(WIN_QO)
+
+        print("---> Creating", WIN_QO)
+        os.mkdir(WIN_QO)
+        print("---> Changing cwd to", WIN_QO)
+        os.chdir(WIN_QO)
+        print("---> Downloading QoLang binary")
+        urllib.request.urlretrieve(
+            "https://github.com/QoLang/QoLang/releases/latest/download/qo.exe", "qo.exe")
+
+        # TODO: find alternative to svn
+        print("---> Downloading built-in libraries")
+        # os.system("svn export https://github.com/QoLang/QoLang/trunk/libs")
+
+        print("--!> TODO: find alternative to svn. Skipping... So, you have to download the libs manually, and copy them to C:\\qolang\\libs")
+        print("---> https://github.com/QoLang/QoLang/tree/master/libs")
+
+        print("---> Adding qolang to PATH")
+        try:
+            os.system("setx PATH \"%PATH%;"+WIN_QO+'"')
+        except:
+            print("--!> Allready added to PATH")
+ 
+        print("---> Done!")
+
 else:
     print("Your operating system is not supported yet. Please follow the instructions on the website.")
+    print("https://qolang.camroku.tech/#Install")
     exit(1)
